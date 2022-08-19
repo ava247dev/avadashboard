5581cfa57a4cd2085b73dd001a5f8b804f6e4418
ava247dev <support@ava-247.com>
2022-08-19 16:00:44 +0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -15,9 +15,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', ''),
+      database: env('DATABASE_NAME', 'avaspeed_dashboard'),
+      user: env('DATABASE_USERNAME', 'avaspeed_avadev'),
+      password: env('DATABASE_PASSWORD', '7Eqh~M,crS2g'),
        ssl: false
       //{
       //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
